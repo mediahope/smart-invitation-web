309d4b32b537915c4cafffe8b893e65114586dfd
mediahope <magazrulit@mail.ru>
2024-11-28 01:20:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, send_file
+from flask_cors import CORS
 from PIL import Image, ImageDraw, ImageFont
 import os
 
 app = Flask(__name__)
+CORS(app, origins=["https://mediahope.ru"])
 
 # Пути к файлам
 BASE_DIR = os.path.abspath(".")
